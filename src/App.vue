ac27353098dec25c4e8deeabe8156a621d525a63
Kamil Kerimov <kamil.kerimov@onlyoffice.com>
2023-12-22 20:56:35 +0500

    implement get cats images

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,98 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from "vue";
+import axios from "axios";
+
+let hrefs = ref(['']);
+const request = 'https://api.thecatapi.com/v1/images/search';
+
+const getCats = async (req: string) => {
+	console.log(req);
+	const { data } = await axios(req);
+	if (data.length < 0)
+		return;
+
+	hrefs.value = [];
+
+	console.log(data);
+
+	hrefs.value = data.map((el: any) => {
+		return el.url
+	});
+
+	console.log(hrefs.value);
+}
+
+const getSingleCat = async () => {
+	getCats(request);
+}
+
+const getTenCats = () => {
+	getCats(request + `?limit=10`);
+}
+
+getSingleCat();
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+	<header>
+		<button v-on:click="getSingleCat" class="button-92">Get cat</button>
+		<button v-on:click="getTenCats" class="button-92">Get 10 cats</button>
+	</header>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<main>
+		<img class="cat-image" v-for="href in hrefs" :src="href" />
+	</main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+main {
+	display: flex;
+	flex-direction: column;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.button-92 {
+  --c: #fff;
+  /* text color */
+  background: linear-gradient(90deg, #0000 33%, #fff5, #0000 67%) var(--_p,100%)/300% no-repeat,
+    #004dff;
+  /* background color */
+  color: #0000;
+  border: none;
+  transform: perspective(500px) rotateY(calc(20deg*var(--_i,-1)));
+  text-shadow: calc(var(--_i,-1)* 0.08em) -.01em 0   var(--c),
+    calc(var(--_i,-1)*-0.08em)  .01em 2px #0004;
+  outline-offset: .1em;
+  transition: 0.3s;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.button-92:hover,
+.button-92:focus-visible {
+  --_p: 0%;
+  --_i: 1;
 }
+
+.button-92:active {
+  text-shadow: none;
+  color: var(--c);
+  box-shadow: inset 0 0 9e9q #0005;
+  transition: 0s;
+}
+
+.button-92 {
+  font-weight: bold;
+  font-size: 2rem;
+  margin: 10px;
+  cursor: pointer;
+  padding: .1em .3em;
+}
+
+.cat-image {
+	max-width: 50vw;
+	margin: 10px;
+	border-radius: 30px;
+}
+
 </style>
---
+++ src/components/Controls.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+
+import { ref } from "vue";
+import axios from "axios";
+
+let href = ref("");
+
+const getImage = async () => {
+	const { data } = await axios('https://api.thecatapi.com/v1/images/search');
+	if (data.length < 0)
+		return;
+
+	href.value = data[0].url;
+
+	console.log(href);
+}
+
+</script>
+
+<template>
+	<button v-on:click="getImage">Get image</button>
+	<input type="number" id="limit" name="limit" min="1" max="25" />
+</template>
+
+<style scoped>
+h1 {
+	font-weight: 500;
+	font-size: 2.6rem;
+	position: relative;
+	top: -10px;
+}
+
+h3 {
+	font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+	text-align: center;
+}
+
+@media (min-width: 1024px) {
+
+	.greetings h1,
+	.greetings h3 {
+		text-align: left;
+	}
+}
+</style>
